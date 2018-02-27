28420f8dd4df34cccaca2795784ff8a722c43647
LipaBhuiyan <Lipabhuiyan1030@gmail.com>
2018-02-27 10:39:06 -0800

    add

---
+++ src/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a=50;
+  int b=5;
+  int c= a+b;
+  System.out.println("sum is="+c);
+	}
+
+}
